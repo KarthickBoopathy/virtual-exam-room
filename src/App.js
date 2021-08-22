3cf9680b49b90cc4b7330960664f73c4cb656396
KarthickBoopathy <Karthickboopathy94@gmail.com>
2021-08-22 15:42:59 +0530

    Creating NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Home from '@material-ui/icons/Home';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import PersonPinIcon from '@material-ui/icons/PersonPin';
 
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+    maxWidth: 500,
+  },
+});
 
 function App() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
 
-  <h1>Machann</h1>
+    <Paper square className={classes.root}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        variant="fullWidth"
+        indicatorColor="secondary"
+        textColor="secondary"
+        aria-label="icon label tabs example"
+      >
+        <Tab icon={<Home />} label="Home" />
+        <Tab icon={<AssignmentIcon />} label="Exam Room" />
+        <Tab icon={<PersonPinIcon />} label="Account" />
+      </Tabs>
+    </Paper>
 
   );
 }
