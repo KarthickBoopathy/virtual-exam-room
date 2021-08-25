df0f86110c4f83e07a9336e8be5804c8389c60cc
KarthickBoopathy <Karthickboopathy94@gmail.com>
2021-08-25 07:44:18 +0530

    Making the Landing Tabs

--- src/App.js
+++ src/App.js
@@ -1,44 +1,8 @@
 import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Home from '@material-ui/icons/Home';
-import AssignmentIcon from '@material-ui/icons/Assignment';
-import PersonPinIcon from '@material-ui/icons/PersonPin';
-
-const useStyles = makeStyles({
-  root: {
-    flexGrow: 1,
-    maxWidth: 500,
-  },
-});
+import ApplicationTab from "./components/Tabs";
 
 function App() {
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-  return (
-
-    <Paper square className={classes.root}>
-      <Tabs
-        value={value}
-        onChange={handleChange}
-        variant="fullWidth"
-        indicatorColor="secondary"
-        textColor="secondary"
-        aria-label="icon label tabs example"
-      >
-        <Tab icon={<Home />} label="Home" />
-        <Tab icon={<AssignmentIcon />} label="Exam Room" />
-        <Tab icon={<PersonPinIcon />} label="Account" />
-      </Tabs>
-    </Paper>
-
-  );
+  return <ApplicationTab />;
 }
 
 export default App;
